1da64a9399c2843918e5d2d741ee9f2fececc2e7
TnSyahril <87937438+TnSyahril@users.noreply.github.com>
2021-07-28 09:50:21 +0700

    Add files via upload

---
+++ arra.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+clear
+toilet 'HALO ARRA' | lolcat
+toilet 'APA KABAR HEHE' | lolcat
+toilet 'OHHH AKU' | lolcat
+toilet 'MAU NGOMONG' | lolcat
+sleep 1
+toilet 'AKU ITU EMM' | lolcat
+sleep 1
+toilet 'GA JADI DEH' | lolcat
+sleep 1
+toilet 'EH JADI DEH' | lolcat
+sleep 1
+toilet 'AKU CMN MO BLANG' | lolcat
+sleep 1
+toilet 'MAKASIH YA UDH BUAT AKU BAHAGIA' | lolcat
+sleep 1
+toilet 'BLS NYA DI WA AJA YAH HEHE' | lolcat
+sleep 1
+toilet 'OOO IYA 1 LAGI HEHE' | lolcat
+sleep 1
+toilet 'vn ke tadi malam yah pliss' | lolcat
+toilet 'pliss banget' | lolcat
+toilet 'gw tunggu yah byee' | lolcat
+toilet 'BAYY BBY' | lolcat
+exit
+
